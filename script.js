6303336de29dc720662755a026b4f60dac84e481
ophza <jonathanlet@icloud.com>
2020-10-09 17:16:47 -0600

    Removed need for prompts and confirms, added clear password button

--- script.js
+++ script.js
@@ -4,39 +4,44 @@ correctLength = false;
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
+var clearBtn = document.getElementById("clear");
 var finalPassword = "";
+var passwordCriteria = [];
+
+function clearPassword(){
+  location.reload();
+}
 
 function generatePassword(){
   // Code to determine the password criteria
   while(!correctLength){
-    var passwordLengthConfirm = prompt("How long would you like your password (between 8 and 128 characters please)");
+    var passwordLengthConfirm = document.getElementById("lengthInput").value;
     var passwordLength = parseInt(passwordLengthConfirm);
     if (passwordLength >= 8 && passwordLengthConfirm <= 128){
       correctLength = true;
     }
     else{
       alert("Inccorect password length please try again!");
+      location.reload();
     }
   }
-  var lowercaseConfirm = confirm("Would you like lowercase letters in your password?");
-  var uppercaseConfirm = confirm("Would you like uppercase letters in your password?");
-  var numConfirm = confirm("Would you like numbers in your password?");
-  var specCharConfirm = confirm("Would you like special characters in your password?");
-
-  var passwordCriteria = []
+  var lowercaseConfirm = document.getElementById("lowercaseConfirm").checked;
+  var uppercaseConfirm = document.getElementById("uppercaseConfirm").checked;
+  var numConfirm = document.getElementById("numConfirm").checked;
+  var specCharConfirm = document.getElementById("specCharConfirm").checked;
 
   if(lowercaseConfirm){
     passwordCriteria.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", 
     "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
   }
-  else if(uppercaseConfirm){
+  if(uppercaseConfirm){
     passwordCriteria.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", 
     "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
   }
-  else if(numConfirm){
+  if(numConfirm){
     passwordCriteria.push("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
   }
-  else if(specCharConfirm){
+  if(specCharConfirm){
     passwordCriteria.push("!", "@", "#", "$", "%", "^", "&", "*", "?", "_", "+", "=", "<", ">", ".", ":", ";", ",", "~")
   }
 
@@ -50,4 +55,5 @@ function generatePassword(){
 }
 
 // Add event listener to generate button
+clearBtn.addEventListener("click", clearPassword);
 generateBtn.addEventListener("click", generatePassword);
